86ebd8ebb7b9d08d2e066377fcbbc954d3af84f5
betsy18 <betsy_dessire@hotmail.com>
2017-11-29 14:08:59 -0500

    añade funcionalidad 2

--- js/app.js
+++ js/app.js
@@ -1,46 +1,48 @@
 
 var showHide = function(e) {
-    var tabSelected = e.target.dataset.tabSelected;
-    var overview = document.getElementById('container-overview');
-    var students = document.getElementById('container-students');
-    var teachers = document.getElementById('container-teachers');
+  var tabSelected = e.target.dataset.tabSelected;
+  var overview = document.getElementById('container-overview');
+  var students = document.getElementById('container-students');
+  var teachers = document.getElementById('container-teachers');
 
-    if(tabSelected === 'tabOverview') {
-        console.log('This is overview section');
-        //ocultar students, teachers  
-        students.style.display ='none';
-        teachers.style.display ='none';
-        // mostrar solo overview
-        overview.style.display = 'block';
-    }else if(tabSelected === 'tabStudents') {
-        console.log('This is students section');
-        //ocultar overview, teachers
-        overview.style.display ='none';
-        teachers.style.display ='none';
-        // mostrar solo students
-        students.style.display ='block';
-    }else if(tabSelected === 'tabTeachers') {
-        console.log('This is teachers section');
-        //ocultar overview, students 
-        overview.style.display ='none';
-        students.style.display ='none';
-        // mostrar solo teachers
-        teachers.style.display = 'block';
-    }
-}
+  if (tabSelected === 'tabOverview') {
+    console.log('This is overview section');
+    // ocultar students, teachers  
+    students.style.display = 'none';
+    teachers.style.display = 'none';
+    // mostrar solo overview
+    overview.style.display = 'block';
+  } else if (tabSelected === 'tabStudents') {
+    console.log('This is students section');
+    // ocultar overview, teachers
+    overview.style.display = 'none';
+    teachers.style.display = 'none';
+    // mostrar solo students
+    students.style.display = 'block';
+  } else if (tabSelected === 'tabTeachers') {
+    console.log('This is teachers section');
+    // ocultar overview, students 
+    overview.style.display = 'none';
+    students.style.display = 'none';
+    // mostrar solo teachers
+    teachers.style.display = 'block';
+  }
+};
 
-var loadPage = function(){
-    var overview = document.getElementById('container-overview');
-    var students = document.getElementById('container-students');
-    var teachers = document.getElementById('container-teachers'); 
-    overview.style.display ='none';
-    students.style.display ='none';
-    teachers.style.display ='none';
-    var tabElements = document.getElementsByClassName('tab');
-    for(var i = 0;i < tabElements.length; i++) {
-        tabElements[i].addEventListener('click', showHide)
-    }
-}
+var loadPage = function() {
+  var overview = document.getElementById('container-overview');
+  var students = document.getElementById('container-students');
+  var teachers = document.getElementById('container-teachers'); 
+  overview.style.display = 'none';
+  students.style.display = 'none';
+  teachers.style.display = 'none';
+  var tabElements = document.getElementsByClassName('tab');
+  for (var i = 0;i < tabElements.length; i++) {
+    tabElements[i].addEventListener('click', showHide);
+  }
+};
+
+alert('probando rama de');
 loadPage();
 // Puedes hacer uso de la base de datos a través de la variable `data`
 // console.log(data);
